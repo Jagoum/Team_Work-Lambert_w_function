5c4eb22a9c004bf8978f8ecbda976d11b97b4639
jagoum <jagoum24@gmail.com>
2025-02-25 15:35:26 +0100

    fix : Updated main to test valid inouts and outputs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use env_logger;
+
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    
 }
+
+#[test]
+fn correct_output(){
+    assert_eq!(1,1)
+}
+
+#[test]
+fn invalid_output(){
+    assert_ne!(2,3,"Two is not eqaul to 3");
+}
+ #[test]
+ fn correct_input(){
+    assert!(true);
+ }
